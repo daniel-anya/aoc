9d90f3c445fd61819f94738b9d71b560b609c7c9
Daniel Anya <daanya72@MBP-1221.ad.avant.com>
2019-12-16 16:00:37 -0600

    solved the second part of the challenge

--- 2019/day_one/aoc_day_one.js
+++ 2019/day_one/aoc_day_one.js
@@ -3,8 +3,18 @@ const fs = require('fs')
 const filePath = String(__dirname + '/input.txt')
 const input = fs.readFileSync(filePath).toString().split('\n').map(value => parseInt(value))
 
-const calculateFuelRequirementFromMass = function(mass){
+const calculateStaticFuelRequirement = function(mass){
     return Math.floor(mass/3) - 2
+}
+
+const calculateDynamicFuelRequirement = function(mass){
+    let fuelRequirementAccumulator = 0
+    let fuelRequirementRemnant = mass
+    while (fuelRequirementRemnant > 0){
+        fuelRequirementRemnant = Math.floor(fuelRequirementRemnant/3) - 2
+        fuelRequirementAccumulator = fuelRequirementRemnant > 0 ? fuelRequirementAccumulator + fuelRequirementRemnant:fuelRequirementAccumulator + 0
+    }
+    return fuelRequirementAccumulator
 }
 
 const calculateTotalFuelRequirement = function(masses, callback){
@@ -13,5 +23,11 @@ const calculateTotalFuelRequirement = function(masses, callback){
       }, 0)
 }
 
-console.log(calculateTotalFuelRequirement(input, calculateFuelRequirementFromMass))
+// part one
+console.log(calculateTotalFuelRequirement(input, calculateStaticFuelRequirement))
+
+// part two
+console.log(calculateTotalFuelRequirement(input, calculateDynamicFuelRequirement))
+
+
 
