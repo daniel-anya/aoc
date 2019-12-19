676c9affea9364c207081833a134fa5e095b3ba6
Daniel Anya <daanya72@MBP-1221.ad.avant.com>
2019-12-19 11:48:46 -0600

    completed part two

--- 2019/day_three/day_three.js
+++ 2019/day_three/day_three.js
@@ -51,39 +51,50 @@ class WireTemplate {
   }
 }
 const crossedWires = (function(dataInput) {
+  const intersections = {};
   const wireOneInput = dataInput[0].split(",");
-
-  //   const wireOneInput = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
-  //   const wireTwoInput = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
-
   const wireTwoInput = dataInput[1].split(",");
 
   const wireOnePath = new WireTemplate(wireOneInput);
   const wireTwoPath = new WireTemplate(wireTwoInput);
 
-  function findIntersections(wireOne, wireTwo) {
+  function _findIntersections(wireOne, wireTwo) {
     const wireOneKeys = Array.from(wireOne.position.keys());
     const wireTwoKeys = Array.from(wireTwo.position.keys());
-    return wireOneKeys.filter(val => wireTwoKeys.includes(val));
+    intersections["key"] = wireOneKeys.filter(val => wireTwoKeys.includes(val));
   }
 
-  function _minimumIntersection(intersections) {
+  function _minimumIntersectionPoint(intersections) {
     const summedIntersections = intersections.map(pos => {
       let split = pos.split(",");
-      let splitArray = split.map(val => parseInt(val));
+      const splitArray = split.map(val => parseInt(val));
       return splitArray.reduce((acc, initVal) => {
         return Math.abs(acc) + Math.abs(initVal);
       }, 0);
     });
-    console.log(intersections);
-    console.log(summedIntersections);
+
     return Math.min(...summedIntersections);
   }
+  function _minimumIntersectionSteps(intersections, wireOne, wireTwo) {
+    // const intersections = _findIntersections(wireOne, wireTwo);
+    const steps = intersections.map(intersection => {
+      return (
+        wireOne.position.get(intersection) + wireTwo.position.get(intersection)
+      );
+    });
+
+    return Math.min(...steps);
+  }
+  _findIntersections(wireOnePath, wireTwoPath);
   return {
-    partOneAnswer: _minimumIntersection(
-      findIntersections(wireOnePath, wireTwoPath)
+    partOneAnswer: _minimumIntersectionPoint(intersections["key"]),
+    partTwoAnswer: _minimumIntersectionSteps(
+      intersections["key"],
+      wireOnePath,
+      wireTwoPath
     )
   };
 })(input);
 
-console.log("Part One answer is ", crossedWires.partOneAnswer);
+console.log("Part One answer is", crossedWires.partOneAnswer);
+console.log("Part Two answer is", crossedWires.partTwoAnswer);
